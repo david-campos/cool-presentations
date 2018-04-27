4a36e1c29721f5fea0d9a5bde404332a8c973a70
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-04-28 00:15:50 +0200

    Probado
    Comprobado que funciona mejor el pdf.js poniendo el link a su github que con la versión descargada, eliminados por tanto.

--- public_html/index.php
+++ public_html/index.php
@@ -1,22 +1,48 @@
 <!doctype html>
-<html lang="es">
+<html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
+	
+	<!-- Our CSS -->
+	<link rel="stylesheet" href="css/main-style.css">
+	
+	<!-- For Bootstrap on IE 9 -->
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Holi, mundi!</h1>
+	<div class="container-fluid">
+		<h1>Holi, mundi!</h1>
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<canvas id="the-canvas"></canvas>
+			</div>
+		</div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<div class="row">
+			<button id="prev">Previous</button>
+			<button id="next">Next</button>
+			&nbsp; &nbsp;
+			<span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
+		</div>
+	
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<!-- pdf.js -->
+	<script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
+	<!-- Rendering control -->
+	<script src="js/rendering_pdf_test.js"></script>
   </body>
 </html>
