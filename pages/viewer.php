93e8f97ecdeb58900777b65a6d429500828fbfac
Unknown <ruben.bagan@gmail.com>
2018-05-12 15:35:48 +0200

    Added presentationControl.js
    Now you are able to swipe left or right, scroll up/down or use arrow keys to navigate through the pdf. However the swipe method it's called too many times, need to be fixed soon.

---
+++ pages/viewer.php
@@ -0,0 +1,14 @@
+<main role="main" class="container-fluid" id='window'>
+	<div class="row justify-content-center">
+		<div class="col text-center">
+			<canvas id="the-canvas"></canvas>
+		</div>
+	</div>
+
+	<div class="row justify-content-center">
+		<button class="btn-primary" id="prev">Previous</button> &nbsp;
+		<button class="btn-primary" id="next">Next</button>
+		&nbsp; &nbsp;
+		<span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
+	</div>
+</main>
