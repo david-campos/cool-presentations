c94cad0c5495179d3d923c7b73f3304b095a9647
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-05-19 21:31:33 +0200

    Tiny bugfix
    Bugfixxxx

--- pages/viewer_scripts.php
+++ pages/viewer_scripts.php
@@ -1,15 +1,15 @@
-<?php
-    $jsonPolls = json_encode($polls, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    if($jsonPolls === false) {
-        $jsonPolls = json_encode(array("jsonError", json_last_error_msg()));
-    }
-    $jsonPres = json_encode($presentation, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    if($jsonPres === false) {
-        $jsonPres = json_encode(array("jsonError", json_last_error_msg()));
-    }
-    
-?>
 <?php if($presentation !== null): ?>
+    <?php
+        $jsonPolls = json_encode($polls, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        if($jsonPolls === false) {
+            $jsonPolls = json_encode(array("jsonError", json_last_error_msg()));
+        }
+        $jsonPres = json_encode($presentation, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        if($jsonPres === false) {
+            $jsonPres = json_encode(array("jsonError", json_last_error_msg()));
+        }
+        
+    ?>
     <script src="js/presentationControl.js"></script>
     <script language="JavaScript" type="text/javascript">
         const PRES = <?php echo $jsonPres; ?>;
