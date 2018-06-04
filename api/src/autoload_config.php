aa75ffb8e56d8ef89a9d3955605898dce326cab2
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-06-04 12:33:38 +0200

    Added autoload configuration and API index
    Autoload configuration to make autoload of the classes without needing requires.
    API index to give general access to the API.

--- api/src/autoload_config.php
+++ api/src/autoload_config.php
@@ -7,15 +7,13 @@
  */
 
 // Defines for directories
-define('CLASS_DIR',  realpath(dirname(__FILE__).'/src/'));
-define('TEST_DIR',  realpath(dirname(__FILE__).'/srcTest/'));
+define('CLASS_DIR',  realpath(dirname(__FILE__)));
+define('TEST_DIR',  realpath(dirname(__FILE__).'/../srcTest/'));
 
 // Changes to the include path of autoload
 set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
 // For unitary tests
-if(defined('TESTING')) {
-    set_include_path(get_include_path().PATH_SEPARATOR.TEST_DIR);
-}
+set_include_path(get_include_path().PATH_SEPARATOR.TEST_DIR);
 
 // Extension
 spl_autoload_extensions('.php');
---
+++ api/src/public_html/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File to give access to the API.
+ * The server should be configured accordingly to redirect the URLs
+ * to this file to process them here.
+ * @author David Campos R. <david.campos.r96@gmail.com>
+ */
+
+// The config file configures the autoloaders for the classes
+require('../autoload_config.php');
+
+// The call to the access controller will be here
